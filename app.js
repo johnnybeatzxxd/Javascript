9b58816d2cb2c5c516dd814df3d0227cc6ba8c3a
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-05-07 14:55:14 +0300

    made the start function functional

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ const buttons = document.querySelectorAll(".button-container button");
 console.log(buttons);
 for (let button of buttons) {
     button.addEventListener("click", function() {
-        button.textContent == "Start" ? start() : button.textContent == "Stop" ? stop() : button.textContent == "Pause" ? pause() : null;
+        button.textContent == "Start" ? start() : button.textContent == "Reset" ? Reset() : button.textContent == "Pause" ? pause() : null;
     })
 }
+let seconds = 0;
+let currentTimer = null;
 
-function start() {};
+function start() {
+    seconds = 0;
+    if (currentTimer || currentTimer != null) Reset();
+    currentTimer = setInterval(timer, 1000);
+};
 
-function stop() {};
+function Reset() {
+    clearInterval(currentTimer)
 
-function pause() {};
+};
 
-function timer() {};
+function pause() {
+    console.log(currentTimer)
+};
 
+function timer() {
+    seconds++;
+    time.textContent = seconds;
+
+
+};
+
+console.log(time);
 console.log(time);
